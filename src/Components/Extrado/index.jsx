3315c3bd397062a6600d1ff9d53e4c659d3f0038
leandromenoci <menoci.leandro@gmail.com>
2022-07-20 23:44:17 -0300

    Vantagens de usar componentes para criar layouts

---
+++ src/Components/Extrado/index.jsx
@@ -0,0 +1,18 @@
+import { Box, Botao } from '../UI'
+import { extratoLista } from '../../info'
+import Items from '../Items'
+
+export default function Extrato() {
+  return (
+    <Box>
+      {
+        extratoLista.updates.map(({ id, type, from, value, date }) => {
+          return (
+            <Items key={id} type={type} from={from} value={value} date={date} />
+          )
+        })
+      }
+      <Botao>Ver mais</Botao>
+    </Box>
+  )
+}
